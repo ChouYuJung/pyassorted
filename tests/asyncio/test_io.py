e6388a1cff6c02e02f2282650423dcc675d71df9
Allen Chou <f1470891079@gmail.com>
2023-05-04 19:10:45 +0800

    Test aio_open write.

---
+++ tests/asyncio/test_io.py
@@ -0,0 +1,40 @@
+import os
+import random
+import string
+import tempfile
+from pathlib import Path
+
+import pytest
+
+from pyassorted.asyncio.io import aio_open
+
+
+target_filename = "test_io.txt"
+test_filename = "test_aio.txt"
+
+
+def random_string(row: int = 100, length: int = 100) -> str:
+    return "\n".join(
+        [
+            "".join(random.choices(string.ascii_letters + string.digits, k=length))
+            for _ in range(row)
+        ]
+    )
+
+
+@pytest.mark.asyncio
+async def test_aio_open_text_file():
+    with tempfile.TemporaryDirectory() as tmp_dir:
+        target_filepath = Path(tmp_dir).joinpath(target_filename)
+        test_filepath = Path(tmp_dir).joinpath(test_filename)
+        random_text_content = random_string()
+
+        # IO Write
+        with open(target_filepath, "w") as f:
+            f.write(random_text_content)
+
+        # AIO Write
+        async with aio_open(test_filepath, "w") as f:
+            await f.write(random_text_content)
+
+        assert os.stat(target_filepath).st_size == os.stat(test_filepath).st_size
