62f74eb8824f9df8b528c3d9b915da6c08440ac2
Allen Chou <f1470891079@gmail.com>
2023-05-10 16:09:15 +0800

    Test test_merge_json_recursively.

--- pyassorted/io/__init__.py
+++ pyassorted/io/__init__.py
@@ -1,3 +1,4 @@
+import copy
 import glob
 import json
 from pathlib import Path
@@ -7,17 +8,27 @@ from typing import Dict, Generator, List, Text, Tuple, Union
 def merge_data(
     obj_1: Union[Dict, List],
     obj_2: Union[Dict, List],
+    inplace: bool = False,
 ) -> Union[Dict, List]:
-    if isinstance(obj_1, Dict) and isinstance(obj_2, Dict):
+    out = obj_1
+    if inplace is False:
+        out = copy.deepcopy(obj_1)
+        obj_2 = copy.deepcopy(obj_2)
+
+    if isinstance(out, Dict) and isinstance(obj_2, Dict):
         for _k, _v in obj_2.items():
-            if _k in obj_1:
-                obj_1[_k] = merge_data(obj_1[_k], _v)
+            if _k in out:
+                out[_k] = merge_data(out[_k], _v, inplace=inplace)
             else:
-                obj_1[_k] = _v
-    elif isinstance(obj_1, List) and isinstance(obj_2, List):
-        obj_1.extend(obj_2)
+                out[_k] = _v
+
+    elif isinstance(out, List) and isinstance(obj_2, List):
+        out.extend(obj_2)
+
     else:
         raise TypeError(f"Cannot merge {type(obj_1)} with {type(obj_2)}")
+
+    return out
 
 
 def read_json(filepath: Text) -> Union[Dict, List]:
@@ -34,3 +45,12 @@ def read_json_recursively(
         return
     for filepath in glob.iglob(f"{dirpath}/**/*.json", recursive=True):
         yield (filepath, read_json(filepath))
+
+
+def merge_json_recursively(dirpath: Union[Path, Text]) -> Union[Dict, List]:
+    dirpath = Path(dirpath)
+
+    obj = {}
+    for _, _data in read_json_recursively(dirpath):
+        obj = merge_data(obj, _data)
+    return obj
--- tests/io/test_io.py
+++ tests/io/test_io.py
@@ -1,34 +1,39 @@
-import random
-import string
+import json
 import tempfile
 from pathlib import Path
+from typing import Dict, List, Text, Tuple, Union
 
 import pytest
 
-from pyassorted.io import read_json_recursively
+from pyassorted.io import merge_json_recursively, read_json_recursively
 
 
-"".join(random.choices(string.ascii_letters + string.digits, k=6))
+test_json_file_with_data: List[Tuple[Text, Union[Dict, List]]] = [
+    ("aaa.json", {"a": 1}),
+    ("bbb/bbb.json", {"b": [2]}),
+    ("bbb/ccc.json", {"c": 3, "b": [3], "d": {"e": [1]}}),
+    ("ddd/ddd.json", {"d": {"e": [3]}, "b": [4]}),
+]
 
 
 @pytest.fixture
 def temp_dir_with_json_files():
     with tempfile.TemporaryDirectory() as f:
         dir_path = Path(f)
-        nested_dirname = "".join(
-            random.choices(string.ascii_letters + string.digits, k=6)
-        )
-        dir_path.joinpath(nested_dirname).mkdir(parents=True, exist_ok=True)
-        with open(dir_path.joinpath("a.json"), "w") as f:
-            f.write('{"a": 1}')
-        with open(dir_path.joinpath(nested_dirname).joinpath("b.json"), "w") as f:
-            f.write('{"b": 2}')
-        with open(dir_path.joinpath(nested_dirname).joinpath("c.json"), "w") as f:
-            f.write('{"c": 3}')
+        for _path, _data in test_json_file_with_data:
+            filepath = dir_path.joinpath(_path)
+            filepath.parent.mkdir(parents=True, exist_ok=True)
+            with open(filepath, "w") as f:
+                json.dump(_data, f)
         yield dir_path
 
 
 def test_read_json_recursively(temp_dir_with_json_files: Path):
-    filename_json = list(read_json_recursively(temp_dir_with_json_files))
-    assert filename_json
-    assert len(filename_json[0]) == 2
+    filename_json_list = list(read_json_recursively(temp_dir_with_json_files))
+    assert filename_json_list
+    assert len(filename_json_list) == len(test_json_file_with_data)
+
+
+def test_merge_json_recursively(temp_dir_with_json_files: Path):
+    obj = merge_json_recursively(temp_dir_with_json_files)
+    assert obj
