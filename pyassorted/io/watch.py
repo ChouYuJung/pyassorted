80c9cf0b33d525906fcbc101d1de3d80bb930e50
Allen Chou <f1470891079@gmail.com>
2023-05-24 18:33:26 +0800

    Watch file iterator.

---
+++ pyassorted/io/watch.py
@@ -0,0 +1,39 @@
+import time
+from pathlib import Path
+from typing import Generator, Text, Union
+
+
+def watch(
+    filepath: Union[Path, Text], frequency: float = 0.1
+) -> Generator[Union[Path, Text], None, None]:
+    """Watch a file for changes.
+
+    Parameters
+    ----------
+    filepath : Union[Path, Text]
+        Path to file to watch.
+    frequency : float, optional
+        Frequency to check for changes, by default 0.1
+
+    Yields
+    ------
+    Union[Path, Text]
+        Path to file that has changed.
+
+    Examples
+    --------
+    >>> from pyassorted.io import watch
+    >>> for filepath in watch('file.txt'):
+    ...     print(filepath)
+    """
+
+    filepath = Path(filepath)
+    file_mtime = filepath.stat().st_mtime
+
+    while True:
+        file_mtime_now = filepath.stat().st_mtime
+        if file_mtime_now != file_mtime:
+            file_mtime = file_mtime_now
+            yield filepath
+        else:
+            time.sleep(frequency)
