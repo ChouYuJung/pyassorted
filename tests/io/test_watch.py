81a418a8ff30282b7354dbc5977c09faffb08615
Allen Chou <f1470891079@gmail.com>
2023-05-26 19:24:43 +0800

    Build watch_count, async_watch_count functions for concurrent.

--- tests/io/test_watch.py
+++ tests/io/test_watch.py
@@ -1,5 +1,6 @@
 import pytest
 import tempfile
+import time
 from pathlib import Path
 
 from pyassorted.io import async_watch, watch
@@ -8,6 +9,24 @@ from pyassorted.io import async_watch, watch
 def wait_and_write_empty(filepath: Path):
     with open(filepath, "w") as f:
         f.write("")
+
+
+def watch_count(filepath: Path, watch_times: int) -> int:
+    count = 0
+    for _ in watch(filepath, frequency=0.01):
+        count += 1
+        if count >= watch_times:
+            break
+    return count
+
+
+async def async_watch_count(filepath: Path, watch_times: int) -> int:
+    count = 0
+    async for _ in async_watch(filepath, frequency=0.01):
+        count += 1
+        if count >= watch_times:
+            break
+    return count
 
 
 @pytest.fixture
@@ -20,9 +39,11 @@ def temp_dir():
 def test_watch(temp_dir: Path):
     assert temp_dir.exists()
     filepath = temp_dir.joinpath("test.txt")
+    filepath.touch()
 
 
 @pytest.mark.asyncio
 async def test_async_watch(temp_dir: Path):
     assert temp_dir.exists()
     filepath = temp_dir.joinpath("test.txt")
+    filepath.touch()
