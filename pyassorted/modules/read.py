7f9382dbd936ab6dc682e638134907514ea95002
Allen Chou <f1470891079@gmail.com>
2024-08-12 20:47:21 +0800

    ## Enhancements
    * Calculate and include MD5 hash for module source code.
    * Refine type hints using `typing_extensions.TypedDict`.
    * Add docstring to `read_modules`.

--- pyassorted/modules/read.py
+++ pyassorted/modules/read.py
@@ -1,11 +1,15 @@
+import hashlib
 import importlib.machinery
 import importlib.util
-from typing import List, Text, TypedDict
+from typing import List, Optional, Text
+
+from typing_extensions import Required, TypedDict
 
 
-class ModuleParam(TypedDict):
-    module_name: Text  # Module name
-    source_code: Text  # Source code
+class ModuleParam(TypedDict, total=False):
+    module_name: Required[Text]  # Module name
+    source_code: Required[Text]  # Source code
+    md5: Optional[Text]  # MD5 hash
 
 
 def _read_file(file_path: Text) -> Text:
@@ -14,6 +18,8 @@ def _read_file(file_path: Text) -> Text:
 
 
 def read_modules(module_path: Text) -> List[ModuleParam]:
+    """Read the module source code."""
+
     spec = importlib.util.find_spec(module_path)
     if spec is None:
         raise ImportError(f"Module '{module_path}' not found.")
@@ -22,5 +28,6 @@ def read_modules(module_path: Text) -> List[ModuleParam]:
         raise ImportError(f"Module '{module_path}' has no file.")
 
     source_code = _read_file(module.__file__)
+    md5 = hashlib.md5(source_code.encode()).hexdigest()
 
-    return [{"module_name": spec.name, "source_code": source_code}]
+    return [{"module_name": spec.name, "source_code": source_code, "md5": md5}]
